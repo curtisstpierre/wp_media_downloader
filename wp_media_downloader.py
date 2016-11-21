ce8caf8c19e1ae7cab1c13b9c48f9b6cae479a9a
curtisstpierre <curtisstpierre@gmail.com>
2016-11-20 18:17:50 -0800

    Update wp_media_downloader.py

--- wp_media_downloader.py
+++ wp_media_downloader.py
@@ -5,8 +5,6 @@ import requests
 tree = ET.parse('embarquecomapri.wordpress.2016-11-14.post_type-attachment.001.xml')
 root = tree.getroot()
 for item in root.iter('item'):
-    print item[20].text
-for item in root.iter('item'):
     url = item[20].text
     with open(url.split('/')[-1], 'wb') as handle:
         response = requests.get(url, stream=True)
